be13a72761b561b81b73ce0f857b5a8d77813d57
Alan Kehoe <alankehoe111@gmail.com>
2014-02-15 20:51:49 +0000

    Make it angular :)

---
+++ app/js/controllers/controllers.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('fleetonrails.controllers', [])
+
+    .controller('LoginCtrl', ['$scope', function ($scope) {
+
+    }])
+
+    .controller('SecondCtrl', ['$scope', function ($scope) {
+
+    }])
+
+    .controller('ThirdCtrl', ['$scope', function ($scope) {
+
+    }])
---
+++ app/js/directives/directives.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('fleetonrails.directives', [])
+
+    .directive('FirstDirective', function() {
+
+    })
+
+    .directive('SecondDirective', function () {
+
+    })
+
+    .directive('ThirdDirective', function() {
+
+    })
