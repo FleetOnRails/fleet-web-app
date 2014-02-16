a804ff44c6dd2ba518100dac49ae253977ba1088
Krystian Jankowski <krystian.jankowski2@mail.dcu.ie>
2014-02-16 23:43:21 +0000

    Added Angular-ui-Bootstrap component to the lib folder, I decided to use the angluar-bootstrap library so we dont get the JQuary error in the console associated with just using bootstrap.

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('fleetonrails', ['ngRoute', 'fleetonrails.controllers', 'fleetonrails.filters', 'fleetonrails.directives']).
+angular.module('fleetonrails', ['ngRoute','ui.bootstrap' ,'fleetonrails.controllers', 'fleetonrails.filters', 'fleetonrails.directives']).
     config(['$routeProvider', function ($routeProvider) {
         $routeProvider.
             when('/', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'}).
