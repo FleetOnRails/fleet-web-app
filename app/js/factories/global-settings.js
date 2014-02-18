b4007f60ce37a1961c8a248967aac2fb852c9ad8
Alan Kehoe <alankehoe111@gmail.com>
2014-02-18 16:45:23 +0000

    Add some global variables. this will be handy when we change to production environment. just have to change a few variables.

---
+++ app/js/factories/global-settings.js
@@ -0,0 +1,9 @@
+angular.module('fleetonrails.factories.global-settings', [])
+
+    .factory('GlobalSettings', [function () {
+        return {
+            api_base_url : 'http://fleet-api.raven.com',
+            api_client_id : '2abe8af97a1e45ee655b5f19d9fb4977990374c2a2895b4aaa6a9d80aa7edeeb',
+            api_client_secret : '33d91b9efcea015b8acaff960ae49164c15da62ff895a253bbfd819b883ba5f6'
+        };
+    }]);
