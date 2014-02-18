da976580e4d4230e86b05adfceeb7ea83ee7358a
Krystian Jankowski <krystian.jankowski2@mail.dcu.ie>
2014-02-18 20:42:11 +0000

    Toggle controller added

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,4 @@
 angular.module('fleetonrails.controllers', [
-    'fleetonrails.controllers.login-controller'
+    'fleetonrails.controllers.login-controller',
+    'fleetonrails.controllers.toggle-controller'
 ]);
---
+++ app/js/controllers/toggle-controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('fleetonrails.controllers.toggle-controller', [])
+
+    .controller('ToggleCtrl', ['$scope', function ($scope) {
+        $scope.isCollapsed = true;
+
+    }])
+/**
+ * Created by krystian on 18/02/2014.
+ */
