39e0e019f9c5b9706a1c501fcc4c3f4a02847ccc
Alan Kehoe <alankehoe111@gmail.com>
2014-02-18 14:21:17 +0000

    Add refresh token service method

--- app/js/controllers/login-controller.js
+++ app/js/controllers/login-controller.js
@@ -4,9 +4,12 @@ angular.module('fleetonrails.controllers.login-controller', [])
         $scope.login = function () {
             LoginService.loginWithPassword($scope.user.email, $scope.user.password)
                 .success(function (data, status) {
-                    console.log('success', data, status)
+                    localStorage.setItem("access_token", data.access_token);
+                    localStorage.setItem("refresh_token", data.refresh_token);
+                    console.log("access_token", localStorage.getItem("access_token"));
+                    console.log("refresh_token", localStorage.getItem("refresh_token"));
                 }).error(function (data, status) {
                     console.log('fail', data, status)
-                });
+                });;
         }
     }]);
