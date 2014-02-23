1e1ea0cd2b86153b5b27dfc777e0a4041ef38018
Krystian Jankowski <krystian.jankowski2@mail.dcu.ie>
2014-02-23 18:37:25 +0000

    Adden transparent background to the log in page, changed the button to blue, keep the samae colour schema as before. Have a look at the image see if you like it

--- app/js/factories/global-settings.js
+++ app/js/factories/global-settings.js
@@ -3,7 +3,7 @@ angular.module('fleetonrails.factories.global-settings', [])
     .factory('globalSettings', [function () {
         return {
             api_base_url : 'http://fleet-api.raven.com',
-            api_client_id : '2abe8af97a1e45ee655b5f19d9fb4977990374c2a2895b4aaa6a9d80aa7edeeb',
-            api_client_secret : '33d91b9efcea015b8acaff960ae49164c15da62ff895a253bbfd819b883ba5f6'
+            api_client_id : 'b3ddf4ad5757dee76e7eb051cecf6b9b1221016432823bbfd34953347c4960f6',
+            api_client_secret : 'ae5873581a055c2ec35b3e95e669923c0a95e330d4bf70ae111cda4076756a23'
         };
     }]);
