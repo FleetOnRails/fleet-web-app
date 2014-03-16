3cd4e2ca0371bbfe17041c9f99813583fa379b5b
Krystian Jankowski <krystian.jankowski2@mail.dcu.ie>
2014-03-16 18:06:12 +0000

    Added map to a car dashboard, still issues with marker, long and lat are loaded after the map is created, posible fix is to add $watch somewhere

--- app/js/controllers/car-controller.js
+++ app/js/controllers/car-controller.js
@@ -2,6 +2,25 @@ angular.module('fleetonrails.controllers.car-controller', [])
 
     .controller('carController', ['$scope', 'CarsService', '$location', '$routeParams', function ($scope, CarsService, $location, $routeParams) {
         $scope.cars = [];
+        var lat = null
+        var long = null
+
+        console.log('Before markers lati is ' + lat + ' long is ' + long)
+        $scope.myMarkers = [
+            {
+                "latitude":lat,
+                "longitude":long
+            }
+        ];
+
+        $scope.center = {
+            latitude: lat,
+            longitude: long
+        };
+
+        $scope.zoom = 13;
+        $scope.markers = $scope.myMarkers;
+        $scope.fit = true;
 
        $scope.getCars = function() {
            CarsService.get(function (data) {
@@ -16,6 +35,10 @@ angular.module('fleetonrails.controllers.car-controller', [])
         $scope.getCar = function(id) {
             CarsService.show(id, function (data) {
                 $scope.car = data['car'];
+                lat = $scope.car.current_gps_statistic.location.latitude
+                console.log(lat)
+                long = $scope.car.current_gps_statistic.location.longitude
+                console.log(long)
             });
         };
 
@@ -61,4 +84,6 @@ angular.module('fleetonrails.controllers.car-controller', [])
         } else {
             $scope.getCars();
         }
+
+
     }]);
