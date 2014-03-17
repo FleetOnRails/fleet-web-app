1474a86d0a678f15745dd0ae7fd3d40c130dcd0f
Krystian Jankowski <krystian.jankowski2@mail.dcu.ie>
2014-03-17 14:44:33 +0000

    Fuel entries get method implemented in service and controller, prints a list of all the fuel entires in a table

--- app/js/controllers/fuel-controller.js
+++ app/js/controllers/fuel-controller.js
@@ -2,6 +2,8 @@ angular.module('fleetonrails.controllers.fuel-controller', [])
 
     .controller('fuelController', ['$scope', 'FuelService', '$location', '$routeParams', function ($scope, FuelService, $location, $routeParams) {
 
+        $scope.fuel_entries = [];
+
         $scope.addFuel = function(id){
             var attributes = {
                 fuel_entry: {
@@ -17,6 +19,22 @@ angular.module('fleetonrails.controllers.fuel-controller', [])
             FuelService.create(id,attributes, function (fuel_entry) {
                 console.log(fuel_entry)
             })
+        }
+
+        $scope.getFuelEntries = function(id) {
+            FuelService.get(id,function (data) {
+                angular.forEach(data, function (fuel_entries, index) {
+                    angular.forEach(fuel_entries, function(value, index) {
+                        $scope.fuel_entries.push(value.fuel_entry)
+                    })
+                });
+            });
+        };
+
+        if ($routeParams && $routeParams.id) {
+            $scope.getFuelEntries($routeParams.id)
+        } else {
+            console.log('something wrong')
         }
 
     }]);
--- app/js/services/fuel-service.js
+++ app/js/services/fuel-service.js
@@ -5,6 +5,22 @@ angular.module('fleetonrails.services.fuel-service', [])
 
     .factory('FuelService', [ '$http', 'globalSettings', function ($http, globalSettings) {
         return {
+            get: function (id,success) {
+                $http({
+                    method: 'GET',
+                    url: globalSettings.api_base_url + '/v1/cars/'+ id + '/fuel_entries',
+                    headers: {
+                        'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json'
+                    }
+                }).success(function (fuel_entries) {
+                        console.log('success', fuel_entries);
+                        success(fuel_entries);
+                    }).error(function (data) {
+                        console.log(data);
+                    })
+            },
             create: function (id,attributes, success) {
                 $http({
                     method: 'POST',
@@ -15,8 +31,10 @@ angular.module('fleetonrails.services.fuel-service', [])
                         'Content-Type': 'application/json',
                         'Accept': 'application/json'
                     }
-                }).success(function (user) {
-                        success(user);
+                }).success(function (fuel_entries) {
+                        console.log('after succes')
+                        console.log(fuel_entries);
+                        success(fuel_entries);
                     }).error(function (data) {
                         console.log(data);
                     })
