4d3475ee784a1a718e8911a05449fd8b1197b7c8
Krystian Jankowski <krystian.jankowski2@mail.dcu.ie>
2014-03-20 12:43:13 +0000

    Updated form for Fuel entry, select option implemented

--- app/js/controllers/fuel-controller.js
+++ app/js/controllers/fuel-controller.js
@@ -2,7 +2,15 @@ angular.module('fleetonrails.controllers.fuel-controller', [])
 
     .controller('fuelController', ['$scope', 'FuelService', '$location', '$routeParams', function ($scope, FuelService, $location, $routeParams) {
 
+        $scope.options = [{ name: "true", id: 1 }, { name: "false", id: 2 }];
+        $scope.selectedOption = $scope.options[1];
+
+        $scope.optionsFuel = [{ name: "Petrol", id: 1 }, { name: "Diesel", id: 2 }];
+        $scope.selectedOptionFuel = $scope.optionsFuel[1];
+
         $scope.fuel_entries = [];
+
+
 
         $scope.addFuel = function(id){
             var attributes = {
@@ -10,15 +18,17 @@ angular.module('fleetonrails.controllers.fuel-controller', [])
                     odometer: $scope.fuel.odometer,
                     liters: $scope.fuel.liters,
                     price: $scope.fuel.price,
-                    fuel_type: $scope.fuel.fuel_type,
+                    fuel_type: $scope.selectedOptionFuel.name,
                     filling_station: $scope.fuel.filling_station,
-                    filled_tank: $scope.fuel.filled_tank
+                    filled_tank: $scope.selectedOption.name
                 }
             };
+            console.log($scope.selectedOption.name)
             console.log(attributes)
             FuelService.create(id,attributes, function (fuel_entry) {
                 console.log(fuel_entry)
             })
+
         }
 
         $scope.getFuelEntries = function(id) {
