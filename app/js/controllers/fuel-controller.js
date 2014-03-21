5f5872e5e5e904f7bd3ce395de401fb93ba7ecbf
Krystian Jankowski <krystian.jankowski2@mail.dcu.ie>
2014-03-21 15:37:00 +0000

    Average litres graph implemented in fuel entry

--- app/js/controllers/fuel-controller.js
+++ app/js/controllers/fuel-controller.js
@@ -10,17 +10,9 @@ angular.module('fleetonrails.controllers.fuel-controller', [])
 
         $scope.fuel_entries = [];
 
-        $scope.fuel_data = [];
+        $scope.fuel_data = [] ;
 
-
-        $scope.total=function(){
-            var total=0;
-            angular.forEach($scope.fuel_entries, function(fuel_entry) {
-                total += fuel_entry.liters;
-            });
-            console.log("total Fuel is " + total)
-            return total;
-        };
+        $scope.gauge_data = [];
 
 
         $scope.CollapseDemoCtrl = function(){
@@ -28,10 +20,35 @@ angular.module('fleetonrails.controllers.fuel-controller', [])
 
         }
 
-        $scope.gauge_data = [
-            {label: "Fuel", value: 78, color: "#5398f1", suffix: "%"}
-        ];
-        $scope.fuel_options = {thickness: 5, mode: "gauge", total: 100};
+
+        getFuelEntries = function(id) {
+            FuelService.get(id,function (data) {
+                var total=0;
+                var count = 1;
+                $scope.gauge_data = [];
+                angular.forEach(data, function (fuel_entries, index) {
+                    angular.forEach(fuel_entries, function(value, index) {
+                        $scope.fuel_entries.push(value.fuel_entry)
+                        $scope.fuel_data.push(value.fuel_entry.liters)
+                        total += value.fuel_entry.liters;
+                        count = count + 1;
+                    })
+                });
+                $scope.gauge_data.push(
+                    {label: "Fuel", value:total/count, color: "#5398f1", suffix: "L"}
+                )
+            });
+        };
+
+        $scope.deleteEntry = function(id, fuel_id){
+
+            FuelService.delete(id,fuel_id ,function(car){
+                // TODO fixed the table update after deletion
+                //console.log(fuel_entries);
+                //$scope.fuel_entries($scope.fuel_entries.indexOf(fuel_id),1);
+            })
+        };
+
 
         $scope.addFuel = function(id){
             var attributes = {
@@ -55,30 +72,14 @@ angular.module('fleetonrails.controllers.fuel-controller', [])
 
         }
 
-        $scope.getFuelEntries = function(id) {
-            FuelService.get(id,function (data) {
-                angular.forEach(data, function (fuel_entries, index) {
-                    angular.forEach(fuel_entries, function(value, index) {
-                        $scope.fuel_entries.push(value.fuel_entry)
-                        $scope.fuel_data.push(value.fuel_entry.liters)
-                    })
-                });
-            });
-        };
-
-        $scope.deleteEntry = function(id, fuel_id){
-
-            FuelService.delete(id,fuel_id ,function(car){
-                // TODO fixed the table update after deletion
-                //console.log(fuel_entries);
-                //$scope.fuel_entries($scope.fuel_entries.indexOf(fuel_id),1);
-            })
-        };
+        $scope.fuel_options = {thickness: 5, mode: "gauge", total: 100};
 
         if ($routeParams && $routeParams.id) {
-            $scope.getFuelEntries($routeParams.id)
+            getFuelEntries($routeParams.id)
         } else {
             console.log('something wrong')
         }
+
+
 
     }]);
