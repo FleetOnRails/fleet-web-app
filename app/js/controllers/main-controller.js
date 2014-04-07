6a7e55b535face0b5c1d71b375529495489c76db
Krystian Jankowski <krystian.jankowski2@mail.dcu.ie>
2014-04-07 18:55:16 +0100

    Check how close the due date of reminders is , display alert if it is today

--- app/js/controllers/main-controller.js
+++ app/js/controllers/main-controller.js
@@ -6,9 +6,37 @@ angular.module('fleetonrails.controllers.main-controller', [])
 
         $scope.cars = [];
         $scope.reminders = [];
+        $scope.alerts = [];
 
 
-        $scope.pending = true
+        $scope.pending = true;
+
+        $scope.closeAlert = function(index) {
+            $scope.alerts.splice(index, 1);
+        };
+
+        $scope.differenceInDays = function (date,discription,reg) {
+
+            var minutes=1000*60;
+            var hours=minutes*60;
+            var days=hours*24;
+            var today = new Date().getTime()
+            var past = new Date(date)
+            var tmp = past.getTime()
+
+            var recent = Math.round(today/days)
+            var due = Math.round(tmp/days)
+
+            console.log('Time passed from today',recent)
+            console.log('Time passed from due date',due)
+
+            if(recent == due){
+                $scope.alerts.push({msg: '' + discription + ' is due for car ' + reg, type: 'danger'});
+
+            }
+
+
+        };
 
         MeService.get(function (user) {
             $scope.user = user;
@@ -63,6 +91,7 @@ angular.module('fleetonrails.controllers.main-controller', [])
                             angular.forEach(data, function (reminders, index) {
                                 angular.forEach(reminders, function(reminder, index) {
                                     value.car.reminders.push(reminder.reminder);
+                                    $scope.differenceInDays(reminder.reminder.date,reminder.reminder.description,value.car.registration)
                                 })
                             });
                             console.log('car', value.car);
