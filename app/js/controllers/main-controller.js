59ffc065547151734a5835c36b4b14d9b284524b
Krystian Jankowski <krystian.jankowski2@mail.dcu.ie>
2014-04-08 09:36:12 +0100

    Change to hours to be more accurate for reminders

--- app/js/controllers/main-controller.js
+++ app/js/controllers/main-controller.js
@@ -1,17 +1,19 @@
 angular.module('fleetonrails.controllers.main-controller', [])
 
-    .controller('MainCtrl', [ '$scope', 'MeService', 'CarsService','RemindersService','$location', 'loginService', function ($scope, MeService, CarsService,RemindersService,$location, loginService) {
+    .controller('MainCtrl', [ '$scope', 'MeService', 'CarsService','RemindersService','FuelService','$location', 'loginService',
+        function ($scope, MeService, CarsService,RemindersService,FuelService,$location, loginService) {
 
         var attributes = []
 
         $scope.cars = [];
         $scope.reminders = [];
         $scope.alerts = [];
-
-
+        $scope.gauge_data = [];
         $scope.pending = true;
+        $scope.fuel_options = {thickness: 5, mode: "gauge", total: 100};
 
-        $scope.closeAlert = function(index) {
+
+            $scope.closeAlert = function(index) {
             $scope.alerts.splice(index, 1);
         };
 
@@ -19,19 +21,21 @@ angular.module('fleetonrails.controllers.main-controller', [])
 
             var minutes=1000*60;
             var hours=minutes*60;
-            var days=hours*24;
+            //var days=hours*24;
             var today = new Date().getTime()
             var past = new Date(date)
             var tmp = past.getTime()
 
-            var recent = Math.round(today/days)
-            var due = Math.round(tmp/days)
-
-            console.log('Time passed from today',recent)
-            console.log('Time passed from due date',due)
-
+            var recent = Math.round(today/hours)
+            var due = Math.round(tmp/hours)
+            console.log('Recent date', recent)
+            console.log('Due date', due)
             if(recent == due){
-                $scope.alerts.push({msg: '' + discription + ' is due for car ' + reg, type: 'danger'});
+                $scope.alerts.push({msg: '' + discription + ' is due for car ' + reg + ' today', type: 'danger'});
+
+            }
+            else if(due < recent){
+                $scope.alerts.push({msg: '' + discription + ' is Overdue for car ' + reg , type: 'success'});
 
             }
 
@@ -84,8 +88,11 @@ angular.module('fleetonrails.controllers.main-controller', [])
         $scope.getCars = function () {
             CarsService.get(function (data) {
                 angular.forEach(data, function (cars) {
+                    var total=0;
+                    var count = 0;
                     angular.forEach(cars, function (value) {
                         value.car.reminders = [];
+                        $scope.gauge_data = [];
                         $scope.cars.push(value.car)
                         RemindersService.get(value.car.id,function (data) {
                             angular.forEach(data, function (reminders, index) {
@@ -96,7 +103,21 @@ angular.module('fleetonrails.controllers.main-controller', [])
                             });
                             console.log('car', value.car);
                         });
+                        FuelService.get(value.car.id,function (data) {
+                            angular.forEach(data, function (fuel_entries, index) {
+                                angular.forEach(fuel_entries, function(value, index) {
+                                    total += value.fuel_entry.liters;
+                                    count = count + 1;
+                                })
+                            });
+                            if(count == 0){
+                                count = count + 1;
+                            }
+                        });
                     })
+//                    $scope.gauge_data.push(
+//                        {label: "Fuel", value:(total/count).toFixed(2), color: "#5398f1", suffix: "L"}
+//                    )
                 });
             });
         };
