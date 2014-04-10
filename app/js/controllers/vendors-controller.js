1988406dae6b6ddc30c0a5030284559f61b6780d
Krystian Jankowski <krystian.jankowski2@mail.dcu.ie>
2014-04-10 11:12:58 +0100

    Vendors get and create method and services working correctly, updated view for vendors

--- app/js/controllers/vendors-controller.js
+++ app/js/controllers/vendors-controller.js
@@ -6,19 +6,33 @@ angular.module('fleetonrails.controllers.vendors-controller', [])
     .controller('vendorsController', ['$scope', 'VendorsService','$location', '$routeParams', function ($scope,VendorsService,$location, $routeParams) {
 
         $scope.vendors = [];
+        $scope.pending = true
 
         $scope.addVendor = function(){
             var attributes = {
                 vendor: {
                     name: $scope.vendor.name,
                     supplies: $scope.vendor.supplies,
-                    location_attributes : $scope.vendor.location_attributes
+                    location_attributes : {
+                        address:$scope.vendor.location_attributes
+                    }
                 }
             };
+            console.log(attributes);
             VendorsService.create(attributes,function(vendors){
                 console.log(vendors)
             })
         }
+
+        VendorsService.get(function (data) {
+            $scope.venodors = [];
+            angular.forEach(data, function (vendors, index) {
+                angular.forEach(vendors, function(value, index) {
+                    $scope.vendors.push(value.vendor)
+                })
+            });
+            $scope.pending = false;
+        });
 
 
     }]);
