272013709f847f28a6a2316c10875b64ff27f0e2
Krystian Jankowski <krystian.jankowski2@mail.dcu.ie>
2014-04-13 01:35:46 +0100

    Service for groups_cars

--- app/js/services.js
+++ app/js/services.js
@@ -14,5 +14,6 @@ angular.module('fleetonrails.services', [
     'fleetonrails.services.vendors_products-service',
     'fleetonrails.services.expenses_documents-service',
     'fleetonrails.services.groups-service',
-    'fleetonrails.services.groups_users-service'
+    'fleetonrails.services.groups_users-service',
+    'fleetonrails.services.groups_cars-service'
 ]);
---
+++ app/js/services/groups_cars-service.js
@@ -0,0 +1,56 @@
+angular.module('fleetonrails.services.groups_cars-service', [])
+
+    .factory('GroupsCarsService', [ '$http', 'globalSettings', function($http, globalSettings) {
+        return {
+            get: function(id,success, error) {
+                $http({
+                    method: 'GET',
+                    url: globalSettings.api_base_url + '/v1/groups/' + id + '/cars',
+                    headers: {
+                        'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json'
+                    }
+                }).success(function(cars){
+                        success(cars);
+                    }).error(function(cars) {
+                        error(cars)
+                        console.log(cars);
+                    })
+            },
+
+            create: function(id,data, success, error) {
+                $http({
+                    method: 'POST',
+                    url: globalSettings.api_base_url + '/v1/groups/' + id + '/cars',
+                    data: data,
+                    headers: {
+                        'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json'
+                    }
+                }).success(function(cars) {
+                        success(cars);
+                    }).error(function(cars) {
+                        error(cars)
+                        console.log(cars);
+                    })
+            },
+            delete: function(id,carID, success, error) {
+                $http({
+                    method: 'DELETE',
+                    url: globalSettings.api_base_url + '/v1/groups/' + id + '/cars/' + carID,
+                    headers: {
+                        'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json'
+                    }
+                }).success(function(cars) {
+                        success(cars);
+                    }).error(function(cars) {
+                        error(cars);
+                        console.log(cars);
+                    })
+            }
+        }
+    }]);
