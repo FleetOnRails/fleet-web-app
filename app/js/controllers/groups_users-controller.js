83e7f928f5452c26c3338b2e07f75124a53c3780
Krystian Jankowski <krystian.jankowski2@mail.dcu.ie>
2014-04-15 11:58:49 +0100

    Updated user service, list all system users in typehead box, permision field added

--- app/js/controllers/groups_users-controller.js
+++ app/js/controllers/groups_users-controller.js
@@ -1,7 +1,28 @@
 angular.module('fleetonrails.controllers.groups_users-controller', [])
 
-    .controller('GroupsUsersCtrl', [ '$scope', 'MeService', '$location', '$routeParams','loginService','GroupsUsersService',
-        function ($scope, MeService,$location,$routeParams, loginService,GroupsUsersService) {
+    .controller('GroupsUsersCtrl', [ '$scope', 'MeService', '$location', '$routeParams','loginService','GroupsUsersService','UsersService',
+        function ($scope, MeService,$location,$routeParams, loginService,GroupsUsersService,UsersService) {
+
+            $scope.form={};
+
+            $scope.p = {};
+            $scope.perForm = {};
+
+            $scope.systemUsers = [];
+
+            $scope.p.options = [{
+                "id": 40,
+                "name": "Manager"
+            }, {
+                "id": 20,
+                "name": "Driver"
+            }, {
+                "id": 30,
+                "name": "Owner"
+            },{
+                "id": 10,
+                "name": "Reporter"
+            }];
 
             $scope.getGroupUsers = function(id){
                 GroupsUsersService.get(id,function(data){
@@ -9,15 +30,28 @@ angular.module('fleetonrails.controllers.groups_users-controller', [])
                     angular.forEach(data, function (users, index) {
                         angular.forEach(users, function(value, index) {
                             $scope.users.push(value.user)
-                            console.log(value.user)
+                            console.log('Group user',value.user)
                         })
                     });
                 })
             }
             //TODO ADD user
 
+            $scope.getSystemUsers = function(){
+                UsersService.get(function(data){
+                    $scope.systemUsers = [];
+                    angular.forEach(data, function (users, index) {
+                        angular.forEach(users, function(value, index) {
+                            $scope.systemUsers.push(value.user)
+                            console.log('System user',value.user)
+                        })
+                    });
+                })
+            }
+
             if ($routeParams && $routeParams.id) {
                 $scope.getGroupUsers($routeParams.id)
+                $scope.getSystemUsers()
 
             }
 
--- app/js/services/users-service.js
+++ app/js/services/users-service.js
@@ -5,6 +5,23 @@ angular.module('fleetonrails.services.users-service', [])
 
     .service('UsersService', ['$http', 'globalSettings', function ($http, globalSettings) {
         return{
+            get: function (success) {
+                $http({
+                    method: 'GET',
+                    url: globalSettings.api_base_url + '/v1/users',
+                    headers: {
+                        'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json'
+                    }
+                }).success(function (users) {
+                        console.log('success', users);
+                        success(users);
+                    }).error(function (data) {
+                        console.log(data);
+                    })
+            },
+
             create: function(data, success) {
                 $http({
                     method: 'POST',
