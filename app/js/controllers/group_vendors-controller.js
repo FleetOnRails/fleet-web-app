6f1da020c592fc2128643c6ea3951cd19d6a87d3
Krystian Jankowski <krystian.jankowski2@mail.dcu.ie>
2014-04-25 15:35:18 +0100

    Add vendor in controller and added to a view

--- app/js/controllers/group_vendors-controller.js
+++ app/js/controllers/group_vendors-controller.js
@@ -1,6 +1,32 @@
 angular.module('fleetonrails.controllers.groups_vendors-controller', [])
 
-    .controller('GroupsVendorsCtrl', [ '$scope', 'MeService', '$location', '$routeParams','loginService','GroupsService',
-        function ($scope, MeService,$location,$routeParams,loginService,GroupsService) {
+    .controller('GroupsVendorsCtrl', [ '$scope', 'MeService', '$location', '$routeParams','loginService','GroupsService','GroupsVendorsService',
+        function ($scope, MeService,$location,$routeParams,loginService,GroupsService,GroupsVendorsService) {
 
+            $scope.addVendor = function(){
+                var attributes= {
+                    vendor:{
+                        name: $scope.vendor.name,
+                        supplies: $scope.vendor.supplies,
+                        location_attributes:{
+                            address: $scope.vendor.address
+                        }
+                    }
+                }
+                console.log(attributes)
+                GroupsVendorsService.create($routeParams.id,attributes,function(vendor){
+                    console.log('Vendor added ',vendor)
+                })
+
+            }
+
+            $scope.getGroup = function(id){
+                GroupsService.show(id,function(data){
+                    $scope.group = data['group'];
+                })
+            }
+
+            if ($routeParams && $routeParams.id) {
+                $scope.getGroup($routeParams.id)
+            }
         }]);
